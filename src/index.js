5bd23eae7cf2c19b728ada815dae7e81669ab621
Valeriia <valeriastanis@gmail.com>
2021-03-02 23:51:00 +0100

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if(array === undefined) {
+    return 0
+  }
+  let min = 0
+  array.forEach(element => {
+    if (element < min) {
+      min = element
+    }
+  });
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if(array === undefined) {
+    return 0
+  }
+  let max = 0
+  array.forEach(element => {
+    if (element > max) {
+      max = element
+    }
+  });
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if(array === undefined || array.length == 0) {
+    return 0
+  }
+  let sum = 0
+  array.forEach(elem =>{
+    sum+=elem
+  });
+  return sum/array.length;
 }
